275678d02bfcbc484a7eeb9154d70636aefd578b
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-05 09:54:51 -0700

    modify

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %c byte(s)\n", $sizeof(char));
-	printf("Size of an integer: %i byte(s)\n", $sizeof(int));
-	printf("Size of a long: %li byte(s)\n", $sizeof(long int));
-	printf("Size of a long long: %lli byte(s)\n", $sizeof(long long int));
-	printf("Size of a float: %f byte(s)\n", $sizeof(float));
+	printf("Size of a char: %u byte(s)\n", $sizeof(char));
+	printf("Size of an integer: %u byte(s)\n", $sizeof(int));
+	printf("Size of a long: %u byte(s)\n", $sizeof(long int));
+	printf("Size of a long long: %u byte(s)\n", $sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", $sizeof(float));
 	return (0);
 }
