d9ad673937dd971d79e973c3bba60b43ad939fb0
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-05 09:29:00 -0700

    file 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program runs with printf
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
