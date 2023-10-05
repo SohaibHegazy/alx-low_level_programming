91eac69d5223e9ff74d120e3658f902ae7d4b992
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-05 13:37:44 -0700

    modify

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ *
+ * main - starting the program
+ *
+ * Description: a program to print without printf or puts
+ *
+ * Return: always 1
+ */
+
+int main(void)
+{
+	char *s="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	while (*s) putchar (*s++)
+}
