1b83eb51bdc00d9894cfb30cdc72cbd8eba47689
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-05 09:43:26 -0700

    modify

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that returns the size of different data types
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	printf("Size of a char: %c\n" $sizeof(char));
+	printf("Size of a integer: %i\n" $sizeof(int));
+	printf("Size of a long: %li\n" $sizeof(long));
+	printf("Size of a long long: %lli\n" $sizeof(long long));
+	printf("Size of a float: %f\n" $sizeof(float));
+	return (0);
+}
