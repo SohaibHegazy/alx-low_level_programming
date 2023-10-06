0c75fc7bcfdc9d3f92b9085ab3f2970c51249507
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-06 07:42:02 -0700

    modify

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: C program to write alphabet using putchar
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	return (0);
+}
