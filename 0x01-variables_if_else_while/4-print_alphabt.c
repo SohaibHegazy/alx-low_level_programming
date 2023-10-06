5184867b5697672768c4afdf722479618082d4f7
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-06 08:14:14 -0700

    modify

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: C program to write alphabet using putchar
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && != 'e')
+		{
+			putchar(ch);
+			ch++;
+		}
+		else
+		{
+			ch++;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
