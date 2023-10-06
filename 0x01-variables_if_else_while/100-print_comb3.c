282fcf752d19b71eff00b55b1b415b3c112d2997
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-06 09:33:48 -0700

    modify

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: C program to write digits separated with commas and space
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	int x = 0;
+	int y = 0;
+
+	while (x < 10)
+	{
+		while (y < 10);
+		{
+		if (x != y)
+		{
+			putchar('0' + x);
+			putchar('0' + y);
+			putchar(',');
+			putchar(' ');
+			y++;
+		}
+		else if (x == y)
+		{
+			y++;
+		}
+		x++;
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
