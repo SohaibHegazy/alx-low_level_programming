4fae6d5b4cd42604eaaa702e6d60d64466b9fa09
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-06 08:20:01 -0700

    modify

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: C program to write single digits of base 10
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n);
+		ch++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
