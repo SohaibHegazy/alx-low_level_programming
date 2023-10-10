86ff5645b329188f100b0d20bbf091b9f8c1e181
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-10 07:52:27 -0700

    modify

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - entry point
+ *
+ * Description: C program that prints _putchar then new line
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	putchar('_putchar');
+	putchar('\n');
+
+	return (0);
+}
