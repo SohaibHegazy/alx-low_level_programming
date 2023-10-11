8a2d82f4e469864fbf2337359b86d28426550a0e
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-10 18:43:30 -0700

    modify

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - entry point
+ *
+ * Description: print every minute
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int h = 0;
+	int m = 0;
+
+	while (h < 24)
+	{
+		while (m < 60)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((m / 10) + 48);
+			_putchar((m % 10) + 48);
+			_putchar('\n');
+		}
+		h++;
+		m = 0;
+	}
+}
+
