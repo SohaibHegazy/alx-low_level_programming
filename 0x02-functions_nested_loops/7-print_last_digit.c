a0bfd96574f18ed0f1ecc0217b728c2d244f5a4b
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-10 18:15:32 -0700

    modify

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+include "main.h"
+
+/**
+ * print_last_digit - entry point
+ *
+ * Description: prints last digit
+ *
+ * Return: always 0
+ */
+
+int print_last_digit(int n)
+{
+	int i;
+	i = n%10;
+	return (i);
+}
