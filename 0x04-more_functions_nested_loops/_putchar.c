1e7c45c4acb3e486ffba5954705d48bdae3164eb
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-12 14:28:56 -0700

    modify

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ * _putchar - entry point
+ *
+ * Description: put char function
+ *
+ * Return: char
+ *
+ */
+
+extern char ch;
+char _putchar(char ch)
+{
+	putchar(ch);
+}
