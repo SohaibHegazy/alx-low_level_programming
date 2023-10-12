26d634c99779ba2e32aa2dafc08b6e46bd9da345
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-12 15:49:23 -0700

    modify

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - entry point
+ *
+ * Description: draw diagonal
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	while (n--)
+	{
+		_putchar('\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
