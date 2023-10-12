651e3c16b7da73da323e1dd806ac5fe396ff2467
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-12 15:01:59 -0700

    modify

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ *
+ * Description: prints 0-9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int n = 0;
+	while (n < 10)
+	{
+		_putchar(n + 48);
+		_putchar('\n');
+	}
+}
