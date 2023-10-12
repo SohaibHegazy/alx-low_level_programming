94dacdd821dd91597e7ce6ef04fe3db0293fd2bd
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-12 15:12:08 -0700

    modify

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Description: prints 0-9 except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		if (n == 2 || n == 4)
+		{
+			n++;
+		}
+		else
+		{
+			_putchar(n + 48);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
