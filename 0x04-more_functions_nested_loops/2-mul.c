2fbf847a9caeda32444acf42e0df001f6b5f4bed
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-12 14:57:26 -0700

    modify

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ *
+ * Description: multiply two numbers
+ * @a: input
+ * @b:input
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
