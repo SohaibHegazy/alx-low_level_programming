1402ffb4f3aa8ec5a7641ffa9402064094085c4c
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-12 14:00:57 -0700

    modify

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ *
+ * Description: checks if letter is upppercase
+ * @c: character to be checked
+ *
+ * Return: 1 if upper and 0 if else
+ */
+
+int isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1)
+	else
+		return (0);
+}
