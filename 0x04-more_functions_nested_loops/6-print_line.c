c4899f93ce72d3f5863653166bffcdc56a1bff70
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-12 15:45:56 -0700

    modify

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - entry point
+ *
+ * Description: draw st line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	while (n--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
