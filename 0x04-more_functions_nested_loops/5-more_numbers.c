aaa6777bb936f4099f84dab342ac784fb2251523
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-12 15:31:32 -0700

    modify

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ *
+ * Description: write 0 - 14 10 times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int t = 0;
+	int n = 0;
+	int u;
+
+	while (t < 10)
+	{
+		while (n < 15)
+		{
+			if (n > 9)
+			{
+				_putchar(1 + 48);
+			}
+			u = n % 10;
+			_putchar(u + 48);
+			n++;
+		}
+		t++
+	}
+}
