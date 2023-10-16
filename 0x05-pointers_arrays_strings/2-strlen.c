413e294f6c7f9022b621c5cfc38815b4081835e0
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-16 13:09:47 -0700

    modify

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - the function
+ *
+ * Description: length of string
+ * @s: input
+ *
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	for (int i = 0; *s !== '\0'; i++)
+	{
+		char *p = *s + i;
+	}
+	return (p - s);
+}
