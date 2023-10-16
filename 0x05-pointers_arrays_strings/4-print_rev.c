ae894b9ba323c6305482d895fbbe880dfa631c6c
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-16 13:52:51 -0700

    modify

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#iclude "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - the function
+ *
+ * Description: print reverse string
+ * @s: input
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+	while (i >= 0)
+	{
+		_putchar(*s);
+		s--;
+		i--;
+	}
+}
