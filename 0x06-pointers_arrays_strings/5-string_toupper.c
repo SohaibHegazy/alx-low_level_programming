3d0e7eb8bbf0609396dbcb52a130963388b22b03
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-19 03:31:33 -0700

    modify

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include < stdio.h>
+
+/**
+ * *string_toupper - the function
+ *
+ * Description: change lowercase into uppercase
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] > 96 && s[i] < 123)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
+
