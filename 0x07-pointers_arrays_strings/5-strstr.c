a4cf0e59ec52d3827c83dce8fc5ccf1d0f4c355c
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-23 10:48:07 -0700

    modify

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,12 +17,13 @@ char *_strstr(char *haystack, char *needle)
 	for (i = 0; haystack[i] != '\0'; i++)
 	{	
 		for (j = 0; needle[j] == haystack[i]; i++, j++)
-		;
-
+		{
 		if (needle[j] == '\0')
 		{
 			return (&haystack[i - j]);
 		}
-		return ('\0');
+		}
+	}
+	return ('\0');
 }
 
