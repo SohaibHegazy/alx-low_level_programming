2f21dc671d58f799abe1a92499e6287ca7402935
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-25 09:32:52 -0700

    modify

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,7 +16,11 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-	else if (*s1 == *s2 || (*s2 == '*' && *s1 != '\0'))
+	else if (*s2 == '*' && *s1 != '\0')
+	{
+		return (1);
+	}
+	else if (*s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
