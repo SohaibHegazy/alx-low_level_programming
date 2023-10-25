4cc0eb84818e46eadac26155ae7243571a2fcd82
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-24 21:54:49 -0700

    modify

---
+++ 0x08-recursion/putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ * _putchar - entry point
+ *
+ * Description: put char function
+ *
+ * Return: char
+ *
+ */
+
+extern int ch;
+int _putchar(int c)
+{
+        putchar(c);
+}
+~  
