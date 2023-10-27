92a863cc8dcc8fb03e5d3100901f24bcaa3090c6
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-27 11:57:44 -0700

    modify

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * 
+ * Description: get the least amount of coins in change
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if true and 1 if Error
+ */
+
+int main(int argc, const char *argv[])
+{
+	if (argc == 2)
+	{
+		if (*argv[1] < 48)
+		{
+			printf("0\n");
+			return (0);
+		}
+		else
+		{
+			int i, cents = atoi(argv[1]), change = 0;
+			int coin[5] = {25, 10, 5, 2 ,1};
+
+			for (i = 0; i < 5; i++)
+			{
+				if (cents >= coin[i])
+				{
+					change = change + (cents / coin[i]);
+					if (cents % coin[i] == 0)
+					{
+						break;
+					}
+					cents = cents % coin[i];
+				}
+			}
+			printf("%d\n" change);
+			return (0);
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
