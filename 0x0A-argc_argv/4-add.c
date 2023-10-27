23b4d9d44d6e1d265269948151110065f7b3f452
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-27 12:15:40 -0700

    modify

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,26 +15,18 @@ int main(int argc, const char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc < 2)
+	for (i = 1; i < argc; i++)
 	{
-		printf("%d\n", 0);
-		return (0);
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		if (*argv[i] < 48 || *argv[i] > 57)
 		{
-			if (*argv[i] < 48 || *argv[i] > 57)
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				sum = sum + atoi(argv[i]);
-			}
+			printf("Error\n");
+			return (1);
 		}
-		printf("%d\n", sum);
-		return (0);
+		else
+		{
+			sum = sum + atoi(argv[i]);
+		}
 	}
+	printf("%d\n", sum);
+	return (0);
 }
