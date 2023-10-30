bd4cb002914b16d108e32b018b91aa39b3a2eafd
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-30 06:38:17 -0700

    modify

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * ree_grid - the function
+ *
+ * Description: free the allocated memory
+ * @grid: the 2D array
+ * @height: heught of 2D array
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
