17d62a3ccf1101e0b9b903488f03a4a1e78dc114
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-10-30 01:23:46 -0700

    modify

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * alloc_grid - the function
+ *
+ * Description: makes 2D array
+ * @width: no. of columns of 2D array
+ * @height: no. of rows in 2D array
+ *
+ * Return: array pointer or NULL if failure
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int *ptr = NULL;
+	int size = 0;
+
+	if (width <= 48 || height <= 48)
+	{
+		return (NULL);
+	}
+	else
+	{
+		size = width * height;
+		ptr = (int *)calloc(size, sizeof(int));
+		return (ptr);
+	}
+}
