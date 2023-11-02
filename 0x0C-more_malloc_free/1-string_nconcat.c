37b02e6452e3d353414896a9ab6153bc8f4166e4
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-11-02 06:55:29 -0700

    modify

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,6 +21,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int l1, l2;
 	char *ptr;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	l1 = strlen(s1);
 	l2 = strlen(s2);
 
