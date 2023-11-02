212256c31921d9660a1f30eada750973f669d929
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-11-02 08:21:11 -0700

    modify

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - the function
+ *
+ * Description: qllocates memory of nmemb elements of size bytes
+ * @nmemb: number of elements
+ * @size: size of each element
+ *
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL)
+	}
+	else
+	{
+		ptr = malloc(size * nmemb);
+		if (ptr == NULL)
+			return (NULL);
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] =0;
+		}
+		return (ptr);
+	}
+}
