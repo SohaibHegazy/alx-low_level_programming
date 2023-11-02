aae0e5493e438ea7bddac07f6f06e24b4df201e4
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-11-02 09:38:37 -0700

    modify

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,6 +14,9 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int i, min_size;
+	unsigned int *m;
+
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
