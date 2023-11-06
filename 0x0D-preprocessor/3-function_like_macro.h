5d94ddd538a7373133c13e7a29ee99565c439ad1
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-11-06 06:45:57 -0800

    modify

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,9 +1,11 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) if(x < 0)\
+#define ABS(x) {\
+	if(x < 0)\
 	return (-(x))\
 	else\
-	return (x)
+	return (x)\
+}
 
 #endif
