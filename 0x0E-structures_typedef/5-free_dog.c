c9050879f55e834cb8688a4b9653045f37ba35c8
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-11-06 13:32:37 -0800

    modify

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - the function
+ *
+ * Description: free the memory allocation of dog
+ * @d: dog structure
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
