4c2ef1e5eff104dd1595661490a665de427e27d6
SohaibHegazy <sohaib.mohamed@hotmail.com>
2023-11-06 07:54:06 -0800

    modify

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ *
+ * Description: structure with dog details
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char * owner;
+}
+
+#endif
