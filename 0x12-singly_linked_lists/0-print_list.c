2fce1f5cb776a0513017b11d18598d04c770f667
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-16 02:48:40 -0800

    modify

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,13 +10,13 @@
 
 size_t print_list(const list_t *h)
 {
-	list_t n = 0;
+	size_t n = 0;
 
 	while (h)
 	{
 		if (h -> str != NULL)
 		{
-			printf("[%d] %s\n", h -> len, str);
+			printf("[%d] %s\n", h -> len, h -> str);
 			h = h -> next;
 			n++;
 		}
