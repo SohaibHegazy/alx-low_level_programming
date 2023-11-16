95af6ba71a02550a69595276f65fa79686fa1e31
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-16 12:26:34 -0800

    modify

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * free_list - function to delete a list
+ *
+ * @head: pointer to the pointer of first node
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp, *del;
+
+	if (!(*head))
+	{
+		break;
+	}
+	else
+	{
+		temp = *head;
+		del = *head;
+
+		while (temp != NULL)
+		{
+			temp = temp -> next;
+			*head = temp;
+			free(del);
+			del = temp;
+		}
+	}
+}
