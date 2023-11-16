20e38fbd03a1bc8a4d219879d4aed6ff9caf3b81
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-16 13:49:40 -0800

    assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+global _start
+
+section .text
+
+_start:
+	MOV rax, 0x1
+	MOV rdi, 0x1
+	MOV rsi, msg
+	MOV rdx, len
+	SYSCALL
+
+	MOV rax, 0x3c
+	MOV rdi, 0x0
+	SYSCALL
+
+section .data
+
+	msg: DB "Hello, Holberton", 0xA
+	len: EQU $ - msg;
