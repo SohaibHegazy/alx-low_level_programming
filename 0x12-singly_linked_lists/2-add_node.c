e352298591f46d24b30331ed2ba808bdd6806680
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-16 11:43:17 -0800

    modify

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - function to add node at the start of the list
+ *
+ * @head: pointer to the first node
+ * @str: input string
+ *
+ * Return: address of newly added element or NULL if failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+
+	temp = (list_t *)malloc(strlen(str) + 1 + sizeof(int) + sizeof(int));
+
+	if (temp == NULL)
+		return (NULL);
+	else
+	{
+		temp->str = strdup(str);
+		temp->len = strlen(str);
+		temp->next = head;
+		head = temp;
+
+		return (head);
+	}
+}
