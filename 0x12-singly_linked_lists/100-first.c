9172432bd7fa22065674327aabc6ae545b21badc
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-16 12:46:38 -0800

    modify

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * first - function to execute before main
+ *
+ * Return: void
+ */
+
+
+void first()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
