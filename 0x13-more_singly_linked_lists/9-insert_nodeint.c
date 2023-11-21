0f2650cf03e2cfc032a00ffe54e4d1ba5e755c33
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-20 20:55:34 -0800

    modify

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	prev = next = *head;
 
-	if (!head)
+	if (!head || !(*head))
 		return (NULL);
 
 	new = (listint_t *)malloc(sizeof(listint_t));
