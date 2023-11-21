5faa5abc3ebef9cb003b3bd700bdf717e985d4fe
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-20 21:33:20 -0800

    modify

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,11 +39,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			next = next->next;
 			i++;
 		}
-		else if (next && !next->next)
-		{
-			next = next->next;
-			i++;
-		}
 		else
 			return (NULL);
 	}
