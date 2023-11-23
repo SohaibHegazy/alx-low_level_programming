0c8a4980fbd108b660eaa745dce650c978792353
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-23 12:16:56 -0800

    modify

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - function to set a value of a bit to 1
+ *
+ * @n: input int
+ * @index: index of bit to change
+ *
+ * Return: 1 if success or -1 if failed
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	return(!!(*n |= 1L << index));
+}
