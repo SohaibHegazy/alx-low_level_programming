986078a34c19811f2f204ad6a39cf0fd324aaa17
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-23 12:24:28 -0800

    modify

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,5 +14,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(n) * 8)
 		return (-1);
 
-	return (!!(*n &= 1L << index));
+	if (*n & 1L << index)
+		*n ^= 1L << index;
+	return (1);
 }
