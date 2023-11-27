8ed1c5058da0779c7786cc6e4c2be36b65b53b65
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-27 03:13:09 -0800

    modify

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,6 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fp = NULL;
 	size_t i = 0;
+	ssize_t n = 1;
 
 	if (!filename)
 		return (0);
@@ -21,11 +22,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!fp)
 		return (0);
 
-	for (i = 0; i < letters; i++)
+	for (i = 0; i < letters; i++, n++)
 	{
-		char c = fgetc(filename, fp);
-		_putchar(c);
+		char c = fgetc(fp);
+		putchar(c);
 	}
-	return (i + 1);
+	return (n);
 	fclose(fp);
 }
