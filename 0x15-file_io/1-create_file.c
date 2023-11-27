4d612ea1e13224ab87879b527e6076e6123ae721
“SohaibHegazy” <sohaib.mohamed@hotmail.comgit config --global user.email sohaib.mohamed@hotmail.com>
2023-11-27 07:52:49 -0800

    modify

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t size;
+	ssize_t size, len = strlen(text_content);
 
 	if (!filename)
 		return (-1);
@@ -22,10 +22,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (strlen(text_content))
-		size = write(fd, text_content, strlen(text_content));
+	if (len)
+		size = write(fd, text_content, len);
 
-	if (size == strlen(text_content))
+	if (size == len)
 		return (1);
 	else
 		return (-1);
