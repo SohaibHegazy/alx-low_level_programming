c7211b5d94fa97fc04c12b88db23a46f42499275
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-01-11 18:14:37 -0800

    modify

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - finds the length of a doubly linked list
+ * @h: pointer to the list header
+ *
+ * Return: length of the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next
