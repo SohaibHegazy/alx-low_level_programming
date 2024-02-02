2adda9ea42b754c405d20da534a26fa571aa7c67
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-02-02 14:37:16 -0800

    modify

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,5 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -49,3 +51,5 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
+
+#endif
