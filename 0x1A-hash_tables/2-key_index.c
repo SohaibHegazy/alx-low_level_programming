4435205fb3626f1a1e177e86839e4c406953b3ff
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-02-02 15:21:55 -0800

    modify

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function to give the index of a key
+ *
+ * @key: the key
+ * @index: the index
+ *
+ * Return: the index of the key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
