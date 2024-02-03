1f7a1d47674e59655a8d6467c387a2b1443d4e62
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-02-02 16:10:51 -0800

    modify

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,14 +16,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *temp;
 	char *new_value;
 
-	if (ht == NULL || ht->array == NULL || ht->size ==0 ||
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 		key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
-
 	i = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[i];
-
-	while (temp != NULL)
+	for (; temp != NULL; temp = temp->next)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
@@ -32,23 +30,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				return (0);
 			free(temp->value);
 			temp->value = new_value;
-
 			return (1);
 		}
-		temp = temp->next;
 	}
-
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
-
 	node->key = strdup(key);
 	if (node->key == NULL)
 	{
 		free(node);
 		return (0);
 	}
-
 	node->value = strdup(value);
 	if (node->value == NULL)
 	{
@@ -56,9 +49,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(node->key);
 		return (0);
 	}
-
 	node->next = ht->array[i];
 	ht->array[i] = node;
-
 	return (1);
 }
