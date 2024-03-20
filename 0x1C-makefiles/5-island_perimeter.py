75ca524434475dfb091130e443212be8f39289d3
“SohaibHegazy” <sohaib.mohamed@hotmail.com>
2024-03-20 15:50:13 -0700

    modify

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,19 +13,19 @@ def island_perimeter(grid):
     Grid cells are connected horizontally/vertically (not diagonally).
     Grid is rectangular, width and height don’t exceed 100
     '''
+    grid_len = len(grid)
+    list_len = len(grid[0])
     sides = 0
-    for list in grid:
-        for i in list:
-            if i == 0:
-                continue
-            if i == 1:
+    for list in range(0, grid_len):
+        for i in range(0, list_len):
+            if grid[list][i] == 1:
                 sides += 4
-                if grid[list][i - 1] == 1:
-                    sides -= sides
-                if grid[list][i + 1] == 1:
-                    sides -= sides
-                if grid[list - 1][i] ==1:
-                    sides -= sides
-                if grid[list + 1][i] ==1:
-                    sides -= sides
+                if i > 0 and grid[list][i - 1] == 1:
+                    sides -= 1
+                if i + 1 < list_len and grid[list][i + 1] == 1:
+                    sides -= 1
+                if list > 0 and grid[list - 1][i] == 1:
+                    sides -= 1
+                if list + 1 < grid_len and grid[list + 1][i] == 1:
+                    sides -= 1
     return sides
